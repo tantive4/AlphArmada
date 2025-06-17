d2b0036ede73558d251ac979aa49f8962358f214
tantive4 <tantive4@naver.com>
2025-06-17 08:17:14 +0000

    actions set

--- game.py
+++ game.py
@@ -7,18 +7,45 @@ class armada:
     def __init__(self):
         self.player_edge = 30 # 3cm
         self.short_edge = 30 # 3cm
-        self.ship = [] # max 2 per player
-        self.squadron = [] # max 6 per player
+        self.ship = []  # max 3 total, 2 + 1
+        self.squadron = [] # max 10 total, 6 + 4
 
         self.round = 0
         self.roundphase = 0  
         """
           1: ship activation, 2: squadron activation, 
-          3: choose command, 4: command token conversion, 5: squadron command, 6: engineering command,
+          3: choose command, 4: command token conversion, 5: squadron command, 6: engineering command, 7: engineering point spend
           7: set target, 8: add concentrate fire dice, 9: reroll concentrate fire, 10: spend accuracy, 11: spend defense token,
-          12: decide maneuver,
+          12: decide maneuver (speed 1, 2, 3, 4),
           13: squadron movement, 13: squadron set target
         """
+"""
+	possible outcome	
+ship activation	3	
+squadron activation	10	
+choose command	4	
+command token conversion	1	bool
+squadron command	1	bool
+engineering command	1	bool
+engineering point spend	17	< move 4 * 3 + recover 4 + repair 1 (ignore critical)
+set attacking hull	4	
+set target	13	< squadron or ship hull 12
+add dice	3	
+reroll dice	12	< each dice with eye
+spend accuracy	6	< each type, is exhausted
+set attacking hull	4	
+set target	13	< squadron or ship hull 12
+add dice	3	
+reroll dice	12	< each dice with eye
+spend accuracy	6	< each type, is exhausted
+spend defense token	6	< each type, is exhausted (player switch)
+decide maneuver	6	< with caution, each speed joint
+squadron speed	10	< 0.5 distance
+squadron direction	20	< 30deg
+squadron set target	22	< ship hull 12 + squad 10
+"""
+
+
 
     def deploy_ship(self, ship, x, y, direction, speed):
         self.ship.append(ship)
