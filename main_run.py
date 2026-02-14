41499e8dfcc5911c7bac915b739063487336ff79
SeongSu Kim <tantive4@naver.com>
2026-02-14 17:52:17 +0000

    worker loop bug fix

--- main_run.py
+++ main_run.py
@@ -151,7 +151,7 @@ def main():
     if args.mode == "worker":
         while True:
             work(args.worker_id)
-            if args.no_loop : break
+            if args.loop : break
 
     elif args.mode == "trainer":
         download_model(save_best=False)
